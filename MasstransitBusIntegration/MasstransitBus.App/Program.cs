cd22ba5232e2c9438fab8e2ec660e153d713cefa
Mayur Agarwal <mayuragarwal7@outlook.com>
2019-05-22 21:58:12 +0530

    Initial Commit

---
+++ MasstransitBusIntegration/MasstransitBus.App/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace MasstransitBus.App
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
